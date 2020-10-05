6cd8e8e3c78715ddf1e665813a657c4be55db1eb
Ranjan Shrestha <ranjan@sharingapples.com>
2020-10-05 15:27:14 +0545

    Publish v0.1.0

--- server/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-boot-loader \
-  --app "boot-loader-server" \
-  --owner "bhoos" \
-  --type "development" \
-  --directory "/boot-loader-server" \
-  --user "bhoos" \
-  --env "MYSQL_USER=bhoos" "MYSQL_PASS=****"
-
