51d16875f06be1e42b2034382f393be58cc273e6
Ranjan Shrestha <ranjan@sharingapples.com>
2020-10-06 11:34:15 +0545

    Fix update flow in development mode

--- client/src/update.ts
+++ client/src/update.ts
@@ -1,15 +1,22 @@
 import { App, getPackage, getDirectory } from '@bhoos/boot-loader-common';
 import { exec } from 'child_process';
 
-export async function update(app: App) {
-  return new Promise((resolve, reject) => {
-    const cmd = app.type === 'development'
-      ? 'git pull origin master'
-      : `yarn global add ${getPackage(app)}`;
 
-    exec(cmd, { cwd: getDirectory(app) }, (err) => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
+export async function update(app: App) {
+  const cwd = getDirectory(app);
+  async function execAsync(cmd: string) {
+    return new Promise((resolve, reject) => {
+      exec(cmd, { cwd }, (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    })
+  }
+
+  if (app.type === 'development') {
+    await execAsync('git pull origin master');
+    await execAsync('yarn build');
+  } else {
+    await execAsync(`yarn global add ${getPackage(app)}`);
+  }
 }
